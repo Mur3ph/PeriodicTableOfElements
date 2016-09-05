6771ffc11855eb35f9b5ff26a59b8bcf2cb7c2eb
Mur3ph <paulmurphymainstreet@gmail.com>
2016-09-05 12:56:41 +0100

    Added ui view and more logging

--- src/ie/murph/application/App.java
+++ src/ie/murph/application/App.java
@@ -7,6 +7,7 @@ import java.util.Scanner;
 import java.util.logging.Logger;
 
 import ie.murph.model.ReadingElements;
+import ie.murph.view.ElementData;
 
 public class App {
 
@@ -15,14 +16,15 @@ public class App {
 	public static void main(String[] args)
 	{
 		//Print headings data from Map to allow user to choose what they want from the periodic table
-		getPeriodicTableDataHeadings();
+		ElementData element = new ElementData();
+		element.getPeriodicTableDataHeadings();
 		
 		//Get user input
-		System.out.println("Enter the number: ");
+		LOGGER.info("Enter the number: ");
 		int number = M_SCANNER.nextInt();
 		
 		System.out.println("");
-		System.out.println(" *** Periodic elements data *** ");
+		LOGGER.info(" *** Periodic elements data *** ");
 		ReadingElements read = new ReadingElements();
 		
 		// Get name of elements and one column of data from user input choice
@@ -32,7 +34,7 @@ public class App {
 		
 		Map<String, String> mapOfListedData = listsToMap(listOfElementNames, list1);
 		mapOfListedData.forEach((key, value) -> {
-		    System.out.println("Element: " + key + " : Attribute value: " + value);
+			LOGGER.info(" Element: " + key + " : Attribute value: " + value);
 		});
 		
 		//Run the search application again ?
@@ -42,6 +44,8 @@ public class App {
 		System.out.println("");
 //		List<List<String>> list2 = read.readAllElementsFromFile();
 //		list2.forEach(System.out::println);
+		
+		System.exit(0);
 	}
 	
 	// Method to ask the user if they want to try again.
@@ -56,11 +60,13 @@ public class App {
 		}
 		else if (s_continue.equalsIgnoreCase("n") || s_continue.equalsIgnoreCase("no"))
 		{
-			System.out.println("Good bye!");
+			System.out.println("");
+			LOGGER.info("Good bye!");
 			System.exit(0);
 		}
 		else
-			System.out.println("Do you want to run it again: (y/n)");
+			System.out.println("");
+			LOGGER.info("Do you want to run it again: (y/n)");
 			askToContinue();
 			
 	}
@@ -73,35 +79,6 @@ public class App {
 	        map.put(keys.get(i), values.get(i));
 	    }
 		return map;
-	}
-	
-	//Print headings data from Map to allow user to choose what they want from the periodic table
-	private static void getPeriodicTableDataHeadings()
-	{
-		Map<Integer, String> elements = new HashMap<Integer, String>();
-		elements.put(0, "Atomic number");
-		elements.put(1, "Element name");
-		elements.put(2, "Atomic mass in au or g/mol");
-		elements.put(3, "CPK color in RRGGBB hex format");
-		elements.put(4, "Electronic configuration");
-		elements.put(5, "Electronegativity in Pauling");
-		elements.put(6, "Atomic radius in pm");
-		elements.put(7, "Ion radius in pm");
-		elements.put(8, "Van der Waals radius in pm");
-		elements.put(9, "IE-1 in kJ/mol");
-		elements.put(10, "EA in kJ/mol");
-		elements.put(11, "Oxidation states");
-		elements.put(12, "Standard state");
-		elements.put(13, "Bonding type");
-		elements.put(14, "Melting point in K");
-		elements.put(15, "Boiling point in K");
-		elements.put(16, "Density in g/mL");
-		elements.put(17, "Metal or nonmetal?");
-		elements.put(18, "Year discovered");
-		
-		LOGGER.info("*** Headings of data ***");
-		System.out.println(" *** Headings of data *** ");
-		elements.forEach((key, value) -> System.out.println(key + " : " + value));
 	}
 	
 }
---
+++ src/ie/murph/view/ElementData.java
@@ -0,0 +1,42 @@
+package ie.murph.view;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.logging.Logger;
+
+import ie.murph.application.App;
+
+public class ElementData {
+
+	private static final Logger LOGGER = Logger.getLogger(App.class.getName());
+	
+	//Print headings data from Map to allow user to choose what they want from the periodic table
+	public void getPeriodicTableDataHeadings()
+	{
+		Map<Integer, String> elements = new HashMap<Integer, String>();
+		elements.put(0, "Atomic number");
+		elements.put(1, "Element name");
+		elements.put(2, "Atomic mass in au or g/mol");
+		elements.put(3, "CPK color in RRGGBB hex format");
+		elements.put(4, "Electronic configuration");
+		elements.put(5, "Electronegativity in Pauling");
+		elements.put(6, "Atomic radius in pm");
+		elements.put(7, "Ion radius in pm");
+		elements.put(8, "Van der Waals radius in pm");
+		elements.put(9, "IE-1 in kJ/mol");
+		elements.put(10, "EA in kJ/mol");
+		elements.put(11, "Oxidation states");
+		elements.put(12, "Standard state");
+		elements.put(13, "Bonding type");
+		elements.put(14, "Melting point in K");
+		elements.put(15, "Boiling point in K");
+		elements.put(16, "Density in g/mL");
+		elements.put(17, "Metal or nonmetal?");
+		elements.put(18, "Year discovered");
+		
+		LOGGER.info("*** Headings of data ***");
+		System.out.println(" *** Headings of data *** ");
+		elements.forEach((key, value) -> System.out.println(key + " : " + value));
+	}
+	
+}
